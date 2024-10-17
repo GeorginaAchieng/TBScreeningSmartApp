78e375e1a370a6db10a396250b6b777eadc467a1
GeorginaAchieng <georginaodhiambo@yahoo.com>
2024-10-17 11:35:46 +0300

    WelcomeScreen

--- app/src/main/java/com/example/smartapp/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/smartapp/screens/WelcomeScreen.kt
@@ -2,7 +2,6 @@ package com.example.smartapp.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
